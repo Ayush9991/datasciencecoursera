a8f90549cbf93f0a983b00195ad81b49995ded18
Ayush9991 <66176268+Ayush9991@users.noreply.github.com>
2020-07-28 20:03:14 +0530

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,7 @@
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
